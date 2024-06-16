91dfc1a66910ab142c6cb1fac33914706bcef66d
amemome <152237136+Amemome@users.noreply.github.com>
2024-06-16 22:05:20 +0900

    [Platinum III] Title: 피라미드 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/9497. 피라미드 수열/피라미드 수열.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+
+
+using namespace std;
+
+long long gcd(long long x,long long y);
+
+int main(){ 
+
+    long long result = 0;
+    long long N,M;
+    cin >> N >> M;
+
+    if(N == 1 || M == 1) {
+        if(N != 1) {
+            cout << N;
+        } else {
+            cout << M;
+        }
+        return 0;
+    }
+
+    long long ggg = gcd(N-1,M-1);
+
+    long long ratio_N = (N-1)/ggg;
+    long long ratio_M = (M-1)/ggg;
+
+    long long rane = ratio_N*ratio_M*(ggg-1);  // between nodes' length if gcd is 1 no length.
+
+    long long nodes;
+
+
+
+    N = N - ratio_N*(ggg-1);
+    M = M - ratio_M*(ggg-1);
+
+
+
+    if ((N-1)%2 == 0) {
+        nodes = (N)*(M/2);
+    } else if((M-1)%2 ==0) {
+        nodes = (M)*(N/2);
+    } else {
+        nodes = (N)*(M/2);
+    }
+
+    result = nodes + rane;
+
+    
+    cout << result;
+        
+    return 0;
+}
+
+
+long long gcd(long long x, long long y){
+    if(y==0) return x;
+    return gcd(y,x%y);
+}
