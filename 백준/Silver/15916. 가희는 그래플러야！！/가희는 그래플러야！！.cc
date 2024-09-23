7da45ab2c6c45d78934bdd1d1583deef166bee38
amemome <152237136+Amemome@users.noreply.github.com>
2024-09-23 16:10:52 +0900

    [Silver I] Title: 가희는 그래플러야!!, Time: 12 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Silver/15916. 가희는 그래플러야！！/가희는 그래플러야！！.cc
+++ 백준/Silver/15916. 가희는 그래플러야！！/가희는 그래플러야！！.cc
@@ -1,26 +1,43 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<vector>
+
 using namespace std;
 
-using ld = long long;
-const int N = 100010;
-ld n, k;
-ld a[N];
-
-char find() {
-    for (ld i=1; i<=n; i++)
-        if (a[i]==k*i)
-            return 'T';
-    for (ld i=1; i<n; i++)
-        if (a[i]>=k*i && a[i+1]<=k*(i+1))
-            return 'T';
-    return 'F';
-}
+/*
+kx 가 있겠죠. k 의 범위 안에 들어오는지 보면 됨.
+*/
 
 int main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    long long n;
     cin >> n;
-    a[0] = 0;
-    for (int i=1; i<=n; i++)
-        cin >> a[i];
+
+    vector<long long> arr(n+1,0);
+
+    for (int i = 1; i <= n; i++) {
+        cin >> arr[i];
+    }
+
+    long long k;
     cin >> k;
-    cout << find();
+
+
+    for (long long i = 1; i <= n; i++) {
+        if(arr[i] == k*i) {
+            cout << 'T';
+            return 0;
+        }
+    }
+
+    for(long long i = 1; i < n; i++){
+        if(arr[i] >= k*i && arr[i+1] <= k*(i+1)) {
+            cout << 'T';
+            return 0;
+        }
+    }
+    cout << 'F';
+
+    return 0;
 }
