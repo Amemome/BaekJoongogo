0eb17c49c3f6aa031fdedd47174ea3b7127c890a
amemome <152237136+Amemome@users.noreply.github.com>
2024-10-03 16:33:37 +0900

    [Bronze I] Title: 행복 점수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/30445. 행복 점수/행복 점수.cc
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+/*
+길이가 N 인 수열이 주어진다.이때, 다음 쿼리를 수행하
+q 1 Ai 를 v 로 바꿈.
+q 2 i j  Ai ~ Aj 까지에서 크기가 가장 작은 값을 출력. 수열의 인덱스는 1 부터!
+*/
+
+int main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    string s;
+    getline(cin,s);
+
+
+    int hap = 0;
+    int sad = 0;
+
+    for(auto x : s) {
+        for(auto h : "HAPY") {
+            if(h == x){
+                hap+=1;
+            }
+        }
+
+        for(auto d : "SAD") {
+            if(d == x) {
+                sad+=1;
+            }
+        }
+    }
+
+    long long result = 0;
+
+    if(hap == 0 && sad == 0) {
+        result = 50000;
+        cout << result/1000 << ".00";
+        return 0;
+    }
+
+    result =  hap * 100000 / (hap + sad);
+    if(result % 10 >= 5) result += 10;
+
+    result /= 10;
+    
+    double realresult = result / (double) 100;
+
+    cout << fixed;
+    cout.precision(2);
+
+    cout << realresult;
+
+    
+
+    
+
+}
