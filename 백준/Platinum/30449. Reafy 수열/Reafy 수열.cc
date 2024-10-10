5d686643917a6e1a91b4c90b5f4eb53b051fd517
amemome <152237136+Amemome@users.noreply.github.com>
2024-10-10 21:40:33 +0900

    [Platinum V] Title: Reafy 수열, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Platinum/30449. Reafy 수열/Reafy 수열.cc
+++ 백준/Platinum/30449. Reafy 수열/Reafy 수열.cc
@@ -2,79 +2,64 @@
 
 using namespace std;
 
-/*
-브루트 포스 해봐일단.기약분수 <== 니까 어떤 한 수에서의 서로소를 다 구함.
-다 구한 다음에 이걸 정렬하는거지 근데 이게 1/2 기준으로 대칭입니다. 1 -분수 하면 딱 대칭임.
-그럼 그냥 반갈해도 ㄱㅊ은것.
-그럼 서로소를 구하는 범위가 달라집니다. s의 서로소를 구하는데, s
-*/
+int mu[5001];
 
-struct fac{
-    int a,b;
-    fac(int a,int b) : a(a) , b(b) {}   // a/b 의 형태
-    fac() {a = 0; b = 0;}
-};
-
-bool Cmp(const fac& f1, const fac& f2) {
-    return f1.a*f2.b < f1.b*f2.a;
-}
-
-vector<fac> seq = {fac(0,1)};
-
-//서로소를 조금 더 최적화 해서 넣는 함수인데,, 에라토스테네스의 체 변형해서 쓸거임. 루트 m 해서 gcd 되는 것들을 다 처리할까?
-void addprfac(int m) {
-    vector<bool> prime(m/2+1,true);                                 // m 을 2로 나눈것 만큼만 구할거니까 +1 까지 해서 배열을 만들어 준다.
-    prime[0] = false; 
-    
-    for(int i = 1; i  <= m/2; i++) { //gcd 가 1이 아닌것을 봅니다. gcd 가 1 이 아니면 12 2 3 이 나왔을 때, 걍 인수이면 트루나잖음. 이때 걍 박아버리는거.
-        if(prime[i]) {
-            if(__gcd(i,m) != 1) {
-                for(int z = i; z <= m/2; z+=i) {
-                    prime[z] = false;
-                }
-            } else {
-                seq.push_back(fac(i,m));
-            }
+void initmu() {
+    mu[1] = 1;
+    for(int x = 1; x < 5001; x++) {
+        for(int y = 2 * x; y < 5001; y+=x) {
+            mu[y] -= mu[x]; //x에대해영향력뿌리기
         }
     }
 }
 
-//5000 * 2500 번만 돌면 되는데.
 
 
 int main() {
-    cin.tie(NULL);
-    ios_base::sync_with_stdio(false);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    initmu();
 
     int n,k;
     cin >> n >> k;
 
-    /*
-    for(int x = 1; x <= n; x++) {
-        for(int y = 1; y <= x/2; y++) {  
-            if(__gcd(x,y) == 1) {
-                seq.push_back(fac(y,x));
-            }
+    double s = 0, e = 1;
+
+    double m = 0;
+    int c = 45;
+
+    while(c--) {
+        m = (s+e)/2;
+        long long cnt = 0;
+        vector<int> fracs(n+1); //n까지만큼만 볼거니까
+        for(int i = 1; i <= n; i++) {
+            fracs[i] = fracs[i-1] + (int) floor(m*i); //각 fracs 에다가 분모 n인 분수의 누적합저장. 어차피 누적합만 보니까.
+        }
+        //m 보다 왼쪽에 있는 항들의 카운트는. 포함배제원리를 이용하여 처리.
+        //gcd(i,j)=d 가 있을 때, i=d*i' j=d*j'이라 하면 i',j' <= N/d이기때문에
+        for(int i = 1; i <= n; i++) {
+            cnt += fracs[floor(n/i)] * mu[i]; //뫼비우스 함수로 포배원 처리.
+        }
+        //k 번째 항은, k보다 작은 항이 k-1 개 있어야 하므로.
+        if(cnt + 1 >= k) { //이분탐색을 수행한다. 이분탐색을 수행하므로 (1/2)^n 의 배수인 정확도를 갖는 값이 나올거임. 그래서 이거를 거의 무시할정도로 하려면 어떻게 해야할까? 소수 정밀도 미치는 범위까지 반복을 한다 
+            e = m;
+        } else if (cnt + 1 < k) {
+            s = m;
+        }
+        
+    }
+
+    //소수를 어떻게 분수로 만듦? ㄹㅇ
+
+    for(int i = 1 ; i <= n; i++) {
+        if(fabs((i*m)-(round(i*m))) < 0.000000001) {
+            cout << round(i*m) << ' ' << i;
+            return 0;
         }
     }
 
-    */
-
-    for(int x = 1; x <= n; x++) {
-            addprfac(x);
-    }
 
 
-    int siz = seq.size();
-
-    for(int i = 0; i < siz; i++) {
-        seq.push_back(fac(seq[i].b-seq[i].a,seq[i].b)); //그냥 1/2 두개 넣어버리죠? 나중에 처리함 ㅇ
-    }
-
-    if(k > siz) k++;
-    
-    nth_element(seq.begin(),seq.begin()+ k - 1,seq.end(),Cmp);
-    fac result = seq[k-1];
-    cout << result.a << ' ' << result.b ;
-
+    return 0;
 }
