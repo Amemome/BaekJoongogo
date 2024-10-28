ea3bc5bc986fa583fbdc8e9f9fe1255d9aa38171
amemome <152237136+Amemome@users.noreply.github.com>
2024-10-28 22:16:15 +0900

    [Platinum III] Title: 님 게임 나누기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/11872. 님 게임 나누기/님 게임 나누기.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t; cin >> t;
+    long long result = 0;
+    while(t--){
+        int k; cin >> k;
+        if(k%4 == 3) k++;
+        else if(k%4==0)k--;
+        result ^= k;
+    }
+    if(result != 0) cout << "koosaga";
+    else cout << "cubelover";
+}
