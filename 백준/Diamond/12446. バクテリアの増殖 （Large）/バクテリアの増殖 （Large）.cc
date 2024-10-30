29ac9a32f3853141dafe8739f98f64f8bfa03bf8
amemome <152237136+Amemome@users.noreply.github.com>
2024-10-30 12:12:33 +0900

    [Diamond V] Title: バクテリアの増殖 (Large), Time: 40 ms, Memory: 1124 KB -BaekjoonHub

--- 백준/Diamond/12446. バクテリアの増殖 （Large）/バクテリアの増殖 （Large）.cc
+++ 백준/Diamond/12446. バクテリアの増殖 （Large）/バクテリアの増殖 （Large）.cc
@@ -4,38 +4,30 @@
 int memo[1001];
 int phi[1001];
 
-int PHI (int X);
 
 int power_modular (int X, int Y, int M);
 int bacteria_modular (int A, int B, int C);
 
 int main() {
+    for(int i = 0; i < 1001;i++) phi[i] = i;
+    for(int i = 2; i < 1001;i++) {
+        if(phi[i] == i) {
+            for(int j = i; j < 1001;j+=i) {
+                phi[j] = phi[j] * (i-1) / i;
+            }
+        }
+    }
+
+
 	int T;
 	scanf("%d", &T);
 	int A, B, C;
 	for (int i = 1; i <= T; i++) {
 		memset(memo, 0, sizeof(memo));
-		memset(phi, 0, sizeof(phi));
 		scanf("%d %d %d", &A, &B, &C);
 		printf("Case #%d: %d\n", i, bacteria_modular (A, B, C));
 	}
 	return 0;
-}
-
-int PHI (int X) {
-	int answer = X;
-	for (int i = 2; i * i <= X; i++) {
-		if (X % i == 0) {
-			while (X % i == 0) {
-				X /= i;
-			}
-			answer -= answer / i;
-		}
-	}
-	if (X > 1) {
-		answer -= answer / X;
-	}
-	return answer;
 }
 
 int power_modular (int X, int Y, int M) {
@@ -55,11 +47,7 @@ int bacteria_modular (int A, int B, int C) {
 		return power_modular(A, A, C);
 	}
 	int temp = C;
-	while(temp > 1) {
-		phi[temp] = PHI(temp);
-		temp = PHI(temp);
-	}
-	temp = C;
+
 	while(temp > 1) {
 		memo[temp] = power_modular(A, A, temp);
 		temp = phi[temp];
@@ -70,8 +58,10 @@ int bacteria_modular (int A, int B, int C) {
 		while(temp > 1) {
 			K = phi[temp];
 			memo[temp] = power_modular(memo[temp], memo[K] ? memo[K] : memo[K] + K, temp);
-			temp = K;
+            // printf("%d ",memo[temp]);
+            temp = K;
 		}
+        // printf("\n");
 		idx++;
 	}
 	return memo[C];
