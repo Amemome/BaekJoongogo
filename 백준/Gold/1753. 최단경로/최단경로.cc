7c58b89af89b54b975257ef3418d0cd462ab7759
amemome <152237136+Amemome@users.noreply.github.com>
2024-11-08 09:38:25 +0900

    [Gold IV] Title: 최단경로, Time: 84 ms, Memory: 9188 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,40 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-
+int v,e,st; 
 vector<pair<int,int>> adj[20005];
 int d[20005];
 const int INF = INT_MAX;
 
+int dike(int start,int end) {
+    fill(d,d+v+1,INF);
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+    d[start] = 0;
+    pq.push({d[st],st});
+    while(!pq.empty()){
+        auto [dist,cur] = pq.top(); pq.pop();
+        if(d[cur] != dist) continue;
+        for(auto[w,nxt] : adj[cur]) {
+            if(d[nxt] > d[cur] + w) {
+                d[nxt] = d[cur] + w;
+                pq.push({d[nxt],nxt});
+            }
+        }
+    }
+    return d[end];
+}
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    int v,e,st; cin >> v >> e >> st;
-    fill(d,d+v+1,INF);
+    cin >> v >> e >> st;
     while(e--) {
         int u , v , w;
         cin >> u >> v >> w;
         adj[u].push_back({w,v});
     }
-    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-    d[st] = 0;
-    pq.push({d[st],st});
-    while(!pq.empty()){
-        auto cur = pq.top(); pq.pop();
-        if(d[cur.second]!= cur.first) continue; //가중치가 같지 않음?
-        for(auto nxt : adj[cur.second]) {
-            if(d[nxt.second] <= d[cur.second] + nxt.first) continue;
-            d[nxt.second] = d[cur.second] + nxt.first;
-            pq.push({d[nxt.second],nxt.second});
-        }
-    }
+    dike(st,2);
     for(int i = 1; i <= v; i++) {
         if(d[i] == INF) cout << "INF";
         else cout << d[i];
         cout << '\n';
     }
-    
 
 }
