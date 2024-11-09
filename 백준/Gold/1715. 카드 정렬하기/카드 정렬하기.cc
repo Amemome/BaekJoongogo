3cd164a101d93ad43c249db3873a64ddcf8b7da7
amemome <152237136+Amemome@users.noreply.github.com>
2024-11-09 19:41:30 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 24 ms, Memory: 2920 KB -BaekjoonHub

---
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n; cin >> n;
+    priority_queue<int,vector<int>,greater<int>> pq;
+    for(int i = 0; i < n; i++) {
+        int k; cin >> k;
+        pq.push(k);
+    }
+    int result = 0;
+    while(pq.size()>1) {
+        int x = pq.top(); pq.pop();
+        int y = pq.top(); pq.pop();
+        result += x + y;
+        pq.push(x+y);
+    }
+    
+    cout << result;
+
+}
