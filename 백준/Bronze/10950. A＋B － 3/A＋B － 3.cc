6aac7768351227d8eb120da4b59d33181f3eb4cf
amemome <152237136+Amemome@users.noreply.github.com>
2024-11-11 22:55:29 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+vector<int> sieve(1000000,1);
+
+/*
+A ~ B ..
+A 까지의 수.
+M 이 K^N 으로 나누어 떨어지지 않음.
+K의 약수가..
+*/
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+
+    int t; cin >> t;
+    while(t--) {
+        int a,b; cin >> a >>b;
+        cout << a + b << '\n';
+    }
+
+    
+
+}
+
