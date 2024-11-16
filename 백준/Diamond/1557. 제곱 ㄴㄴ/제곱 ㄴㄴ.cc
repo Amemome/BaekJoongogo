b82f5f39f24bcaac50f4d3bb2bbf1438897d694d
amemome <152237136+Amemome@users.noreply.github.com>
2024-11-16 15:17:59 +0900

    [Diamond V] Title: 제곱 ㄴㄴ, Time: 68 ms, Memory: 26744 KB -BaekjoonHub

--- 백준/Diamond/1557. 제곱 ㄴㄴ/제곱 ㄴㄴ.cc
+++ 백준/Diamond/1557. 제곱 ㄴㄴ/제곱 ㄴㄴ.cc
@@ -1,96 +1,79 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include<cmath>
+#include <bits/stdc++.h>
 
-#define ref(i,n) for(int i = 0; i < n; i++)
-#define INF 1e9
 using namespace std;
+#define mod 1000000007
+typedef long long ll;;
 
-long long high = 1700000000;
-long long low = -1;
-long long k;
-int sizer = 0;
-//소수의 제곱을 인수로 갖는 녀석들의 크기를 알고 싶음. 비트마스킹은 너무 커져서 안된다. 재귀 로 ㄱㄱ
-//1700000000  은    1033476056 번째 제곱 ㄴㄴ수. 이정도로 범위 잡으면 될듯
-//탐색하면 search(3) 일 때도 3, search(4) 일 때도 3 임. 그래서 최솟값 찾는 방향으로 ㄱㄱ
+ll pw(ll a,ll b) {ll ret=1;
+    while(b){
+        if(b&1) ret *= a;
+        a*=a;
+        b>>=1;
+    } return ret;
+}
 
-long long rere(int depth , long long index , long long num);
-long long search(long long k);
-vector<long long> prime;
+#define SZ 1000010
 
-void sieve(long long k,vector<long long> & prime){
-
-    k = sqrt(k) + 1;
-    
-    vector<bool> svs(k,1);
-    svs[0] = svs[1] = false;
-
-    for(int i = 2; i*i<k; i++){
-        if(!svs[i]) continue;
-        for(int j = i*i; j < k; j+=i){
-            svs[j] = false;
+vector<int> prime;
+int phi[SZ+1],mu[SZ+1],sigma[SZ+1],tau[SZ+1],e[SZ+1],sp[SZ+1];
+void sieve() {
+    phi[1] = mu[1] =tau[1]=sigma[1] =1;
+    for(int i=2; i<=SZ; i++) {
+        if(!sp[i]){
+            prime.push_back(i);
+            e[i]=1;phi[i]=i-1;mu[i]=-1;tau[i]=2;sigma[i]=i+1;
         }
-        
-    }
-    ref(x,k){
-        if(svs[x]) prime.push_back((long long)x*x);
-    }
-}
-
-long long search(long long k){
-
-    
-    sizer = 0;
-    for(auto x : prime) if (x <= k) sizer++;
-
-
-    long long result = rere(1,0,1);
-    
-    
-    return k - result;
-}
-
-long long rere(int depth, long long index , long long num) { //그냥 k 에 대해서 ㅇㅇ
-
-    long long rst = 0;
-    for(int i = index; i < sizer; i++) {
-        long long bnum = num * prime[i];
-        if(bnum > k) break;
-
-        if(depth % 2 == 0) {
-            rst -= k / bnum;
-        } else {
-            rst += k / bnum;
+        for(auto j:prime){
+            if(i*j >= SZ)break; sp[i*j] = j;
+            if(i%j==0) {
+                e[i*j] = e[i]+1; phi[i*j] = phi[i]*j; mu[i*j]=0;
+                tau[i*j] = tau[i]/e[i*j]*(e[i*j]+1);
+                sigma[i*j] = sigma[i]*(j-1)/
+                (pw(j,e[i*j]-1))*(pw(j,e[i*j]+1)-1)/(j-1);
+                break;
+            }
+            e[i*j]=1; phi[i*j] = phi[i] * phi[j]; mu[i*j] = mu[i] * mu[j];
+            tau[i*j] = tau[i] * tau[j]; sigma[i*j]=sigma[i]*sigma[j];
         }
-        rst += rere(depth+1,i+1,bnum);
     }
-
-    return rst;
+}
+int sumoe[100001];
+void fullmoe() {
+    sumoe[1] = mu[1]; 
+    for(int i = 2; i < 100001; i++) {
+        sumoe[i] = mu[i] + sumoe[i-1];
+    }
 }
 
-int main(){
-    ios::sync_with_stdio(0);
+ll solve(long long k) {
+    ll result = 0; ll j;
+    for(ll i = 1; i * i <= k; i=j+1) {
+        j = k/(k/i);
+        result += (sumoe[j]-sumoe[i-1])*((k/i)/i);
+    }
+    return result;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    sieve(1700000000,prime);
+    sieve();
+    fullmoe();
 
-    long long s;
-    cin >> s; //search 는 k 번째수 라는것을 알려줌..
-    long long r;
+    long long r; cin >> r;
 
-    while(low+1 < high) {
-        long long mid = (high + low) / 2;
-        k = mid;
-        long long nung = search(mid);
-
-        if(s <= nung){
-            high = mid;
-        } else if(s > nung) {
-            low = mid;
-        }
+    
+    long long lo = -1, hi = 2000000000;
         
+    while(lo + 1 < hi) {
+        long long mid =(lo+hi)/ 2;
+        if(solve(mid) < r) lo = mid;
+        else hi = mid; 
     }
-    cout << high;
+
+    cout << hi;
+    
+
     return 0;
 }
